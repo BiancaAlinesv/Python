21c82a89a202ee25d2e783e6e05653c5e8ad8a75
BiancaAlinesv <bianca.alinedev@gmail.com>
2026-04-25 19:04:16 -0300

    feat- atualização atividades

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
 print("Hello World")
+
+nome= "João da Silva"        
+idade= 20
+email= "joao@gmail.com"
+print("Nome:" + nome, "Idade:", idade)
+
+nome=input("digite seu nome")
+print("Olá,", nome, "tudo bem?")
+
+nome=input("digite seu nome:")
+idade=int(input("digite seu idade:"))
+peso=float(input("digite seu peso:"))
+print("O nome digitado é", nome)
+print("A idade digitada é", idade)
+print("O peso digitado é", peso)
